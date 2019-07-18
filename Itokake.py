286e814d6c7d38426c7047f5490be3828388857c
seigo2016 <2003seigo@gmail.com>
2019-07-18 13:46:14 +0900

    add color history

--- Itokake.py
+++ Itokake.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from math import sin, cos
 from PIL import Image, ImageDraw
---
+++ static/js/main.js
@@ -0,0 +1,47 @@
+function change(i) {
+    if (localStorage.getItem('ColorSet' + String(i)) != null) {
+        var getValue = (JSON.parse(localStorage.getItem('ColorSet' + String(i))))
+        for (var i = 0; i < 5; i++) {
+            document.getElementById('color' + String(i + 1)).value = getValue[i]
+        }
+    }
+    console.log(getValue)
+}
+
+function remove() {
+    localStorage.clear()
+}
+function remember() {
+    var getValue = []
+    var len = 0
+    for (var i = 0; i < 6; i++) {
+        if (localStorage.getItem('ColorSet' + String(i)) != null) {
+            getValue.push(JSON.parse(localStorage.getItem('ColorSet' + String(i))))
+            len++;
+        }
+    }
+    if (len == 5) {
+        localStorage.removeItem('ColorSet1');
+        for (var i = 0; i < 5; i++) {
+            localStorage.setItem('ColorSet' + String(i + 1), localStorage.getItem('ColorSet' + String(i + 2)))
+        }
+    }
+    var colorlist = []
+    for (var i = 1; i < 6; i++) {
+        colorlist.push(document.getElementById('color' + String(i)).value)
+    }
+    colorlist.push(document.getElementById('bgcolor').value)
+    localStorage.setItem('ColorSet' + String(len + 1), JSON.stringify(colorlist))
+}
+function getStorage() {
+    var len = 0
+    var getValue = []
+    for (var i = 0; i < 6; i++) {
+        if (localStorage.getItem('ColorSet' + String(i)) != null) {
+            getValue.push(JSON.parse(localStorage.getItem('ColorSet' + String(i))))
+            len++;
+        }
+    }
+    var getjson = localStorage.getItem('ColorSet' + String(len + 1))
+    var getValue = JSON.parse(getjson)
+}
