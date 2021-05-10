26798a263aa46d2d3d3c77c6ca3969881fc6dc20
seigo2016 <2003seigo@gmail.com>
2021-05-10 15:33:29 +0900

    add button

--- dist/main.js
+++ dist/main.js
@@ -3,20 +3,21 @@ const addBtn = document.querySelector('.btn');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (event) => {
+  console.log("show")
   event.preventDefault();
   deferredPrompt = event;
   addBtn.style.display = 'block';
+});
 
-  addBtn.addEventListener('click', (event) => {
-    addBtn.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+addBtn.addEventListener('click', (c) => {
+  addBtn.style.display = 'none';
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
 });
