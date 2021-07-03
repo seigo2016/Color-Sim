dc558ef5ce4ca9798d8ae4a2cb7b5d18ae3dad34
seigo2016 <2003seigo@gmail.com>
2021-07-03 17:30:28 +0900

    update pwa

--- dist/pwabuilder-sw.js
+++ dist/pwabuilder-sw.js
@@ -23,7 +23,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll(offlineFallbackPages))
   );
 });
 
